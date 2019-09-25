53975722b210840ffb945f4e3b1228ece937250f
Nerosen <bzbyyyyf@163.com>
2019-09-25 10:17:28 +0800

    finish

--- src/app.ts
+++ src/app.ts
@@ -12,30 +12,37 @@ const $input = <HTMLInputElement>document.querySelector('.todo-val')
 const $list = <HTMLUListElement>document.querySelector('.list-group')
 const $add = document.querySelector('.button-add')
 
+// 监听用户input事件
 const type$ = Observable.fromEvent<KeyboardEvent>($input, 'keydown')
+  .do(e=> console.log(e,"按输入框事1件"))
   .publish()
   .refCount()
 
+  //监听用户按下回车事件
 const enter$ = type$
   .filter(r => r.keyCode === 13)
+  .do(r => console.log(r,"按下回车获取事件"))
 
 const clickAdd$ = Observable.fromEvent<MouseEvent>($add, 'click')
-
 const input$ = enter$.merge(clickAdd$)
 
 const clearInputSubject$ = new Subject<void>()
 
+//按下回车map节点类比于 Array 的 Map : [ … KeyboardEvent ] => [… HTMLElement 
+//首先在输入回车的时候把 KeyboardEvent map 到 string, filter 掉空值
 const item$ = input$
   .map(() => $input.value)
   .filter(r => r !== '')
   .distinct(null, clearInputSubject$)
   .switchMap(mockHttpPost)
   .map(createTodoItem)
+  //将map出来的节点插入dom
   .do((ele: HTMLLIElement) => {
     $list.appendChild(ele)
     $input.value = ''
     clearInputSubject$.next()
   })
+  .do(r => console.log(r,"转换"))
   .publishReplay(1)
   .refCount()
 
